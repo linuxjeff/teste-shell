5f42709f28d25607cba3f30c381c18ee8152f767
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2022-04-16 15:51:03 -0300

    Copiado programa para testar yes e no do dialog.

---
+++ lsj.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# lsj.sh -- o script do "ls joiado"
+# Este script faz parte do http://aurelio.net/shell/dialog
+
+# Zerando as opções
+cor= ; ocultos= ; subdir= ; detalhes=
+
+# Obtendo as configurações que o usuário deseja
+dialog --yesno 'Usar cores?'               0 0 && cor='--color=yes'
+dialog --yesno 'Mostrar arquivos ocultos?' 0 0 && ocultos='-a'
+dialog --yesno 'Incluir sub-diretórios?'   0 0 && subdir='-R'
+dialog --yesno 'Mostrar visão detalhada?'  0 0 && detalhes='-l'
+
+ls $cor $ocultos $subdir $detalhes
